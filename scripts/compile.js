41c053d384d91f9d3e090c105aaa0510b6b208b9
Robin Beltran <rbeltranmontijo@gmail.com>
2020-03-26 21:48:27 -0600

    test

--- scripts/compile.js
+++ scripts/compile.js
@@ -3,12 +3,14 @@ const fs = require("fs");
 const solc = require("solc");
 const chalk = require("chalk");
 
+// Carga el archivo a contractPath
 const contractPath = path.resolve(
   __dirname,
   "../contracts",
   "UsersContract.sol"
 );
 
+// Lee el codigo fuente
 const source = fs.readFileSync(contractPath, "utf8");
 
 module.exports = solc.compile(source, 1).contracts[":UsersContract"];
--- test/UserContract.test.js
+++ test/UserContract.test.js
@@ -9,7 +9,7 @@ const { interface, bytecode } = require("../scripts/compile");
 
 let accounts;
 let usersContracts;
-console.log(interface);
+
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   usersContracts = await new web3.eth.Contract(JSON.parse(interface))
@@ -17,21 +17,21 @@ beforeEach(async () => {
     .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe("The UsersContract", async () => {
-  it("should deploy", () => {
+describe("Usuario del contrato", async () => {
+  it("Se hizo deploy", () => {
     console.log(usersContracts.options.address);
     assert.ok(usersContracts.options.address);
   });
 
-  it("should join a user", async () => {
+  it("Se unio un usuario", async () => {
     let name = "Robin";
-    let surName = "Montijo";
+    let surname = "Montijo";
     await usersContracts.methods
-      .join(name, surName)
-      .send({ from: accounts[0], gas: "5000000" });
+      .join(name, surname)
+      .send({ from: accounts[0], gas: "500000" });
   });
 
-  it("should retrieve a user", async () => {
+  it("Recuperar usuario", async () => {
     let name = "Robin";
     let surName = "Montijo";
 
@@ -53,7 +53,31 @@ describe("The UsersContract", async () => {
       await usersContracts.methods
         .join("Ana", "Gomez")
         .send({ from: accounts[1], gas: "5000000" });
-      assert.fail("same account cant join twice");
+      assert.fail("la misma cuenta no se puede registrar dos veces");
+    } catch (e) {
+      if (e instanceof AssertionError) {
+        assert.fail(e.message);
+      }
+    }
+  });
+
+  it("should retrieve total registered users", async () => {
+    await usersContract.methods
+      .join("Ana", "Gomez")
+      .send({ from: accounts[0], gas: "500000" });
+
+    await usersContract.methods
+      .join("Mario", "Bros")
+      .send({ from: accounts[1], gas: "500000" });
+
+    let total = await usersContract.methods.totalUsers().call();
+    assert.equal(total, 2);
+  });
+
+  it("should not allow retrieving a not registered user", async () => {
+    try {
+      await usersContract.methods.getUser(accounts[0]).call();
+      assert.fail("user should not be registered");
     } catch (e) {
       if (e instanceof AssertionError) {
         assert.fail(e.message);
